79af07fd0d1bdec9ca31c9c5c3ebccd9e0fffa07
Kumar <kumard4@student.douglascollege.ca>
2023-02-09 17:11:40 -0800

    Added the propertycard and property collection

---
+++ src/components/property/PropertyCard.js
@@ -0,0 +1,10 @@
+function PropertyCard() {
+    return (
+      <div className="Property">
+        <h1>Property</h1>
+      </div>
+    );
+  }
+  
+  export default PropertyCard;
+  
---
+++ src/components/property/PropertyCollection.js
@@ -0,0 +1,10 @@
+function PropertyCollection() {
+    return (
+      <div className="Property">
+        <h1>Property</h1>
+      </div>
+    );
+  }
+  
+  export default PropertyCollection;
+  
