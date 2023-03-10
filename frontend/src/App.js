41fb9392f6b68a982020a428c6250ae8384ca7f9
drake7 <Deepak.kumar7v@gmail.com>
2023-03-10 09:29:30 -0800

    Merge typography

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
-
 //for styling
 // import "./App.css";
 import "./styles/typography.css"
-
 import "./styles/home.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 //for routing
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/home/Home";
 import HomeBar from "./components/nav/HomeBar";
 import Ptcontainer  from "./components/property/PtContainer";
+import Property from "./components/property/Property";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from "react";
 function App() {
