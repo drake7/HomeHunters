f6b7e69b5902762db0e06046de74ece344ca2a63
Aman <amanmishra4191@gmail.com>
2023-03-23 09:01:19 -0700

    added tooltip progress and home header installed react icons

--- frontend/src/components/property/PtCard.js
+++ frontend/src/components/property/PtCard.js
@@ -1,20 +1,32 @@
 import React from "react";
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import Tooltip from "./Tooltip";
 
 function PtCard(props) {
   return (
-    <div className="property-card">
-      <img
-        className="property-img"
-        src={props.image}
-        alt="Property"
-      />
-
-      <div className="property-dts">
-        <h5 className="location">{props.title}</h5>
-        <h3 className="specs">{props.specs}</h3>
-        <h4 className="rent">{props.rent}</h4>
-      </div>
-    </div>
+    <>
+      <Tippy content={<Tooltip {...props} />}
+        arrow={true}
+        delay={500}
+        offset={[0, 20]}
+        maxWidth='28rem'
+        maxHeight='10rem'
+        placement="right">
+        <div className="property-card">
+          <img
+            className="property-img"
+            src={props.image}
+            alt="Property"
+          />
+          <div className="property-dts">
+            <h5 className="location">{props.title}</h5>
+            <h3 className="specs">{props.specs}</h3>
+            <h4 className="rent">${props.rent} per month</h4>
+          </div>
+        </div>
+      </Tippy>
+    </>
   );
 }
 
--- frontend/src/components/property/PtContainer.js
+++ frontend/src/components/property/PtContainer.js
@@ -5,66 +5,116 @@ export default function Ptcontainer() {
       id: 0,
       image:
         "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      title: "Independant house Burnaby, BC",
+      title: "Independant House",
       specs: "2 Beds,1.5 Baths",
-      rent: "$2800 per weeek bitch"
+      rent: "2800",
+      locality: "Royal Oaks",
+      city: "Burnaby",
+      province: "BC",
+      owner: "Samantha",
+      dim: "1250 SqFt",
+      furniture: "Semi-furnished",
+      dts: " Renting the upstairs portion of the home. At walking distance to the skytrain station"
     },
     {
       id: 1,
       image:
         "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80",
-      title: "Haunted house Surrey,BC",
-      specs: "5 Beds,1.5 Baths",
-      rent: "$2800 per weeek bitch"
+      title: "Private Room",
+      specs: "5 Beds, 1.5 Baths",
+      rent: "2800",
+      locality: "Downtown",
+      city: "Vancouer",
+      province: "BC",
+      owner: "Rayan",
+      dim: "1350 SqFt",
+      furniture: "Semi-Furnished",
+      dts: " Renting the upstairs portion of the home. At walking distance to the skytrain station"
     },
 
     {
       id: 2,
       image:
         "https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1530&q=80",
-      title: "Lake house Vancouver,BC",
+      title: "Lake-House",
       specs: "4 Beds,1.5 Baths",
-      rent: "$2800 per weeek bitch"
+      rent: "2800",
+      locality: "Downtown",
+      city: "Vancouer",
+      province: "BC",
+      owner: "Rayan",
+      dim: "1350 SqFt",
+      furniture: "Semi-Furnished",
+      dts: " Renting the upstairs portion of the home. At walking distance to the skytrain station"
     },
     {
       id: 3,
       image:
         "https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1465&q=80",
-      title: "Nashwill house Burnaby,BC",
+      title: "Independant House",
       specs: "1 Beds,1.5 Baths",
-      rent: "$1200 per weeek bitch"
+      rent: "1200",
+      locality: "Downtown",
+      city: "Vancouer",
+      province: "BC",
+      owner: "Rayan",
+      dim: "1350 SqFt",
+      furniture: "Semi-Furnished",
+      dts: " Renting the upstairs portion of the home. At walking distance to the skytrain station"
     },
     {
       id: 4,
       image:
         "https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      title: "Bath house Calgary,AB",
-      specs: "1 Beds,3 Baths",
-      rent: "$2800 per weeek bitch"
+      title: "Private Room",
+      specs: "1 Beds, 3 Baths",
+      rent: "2800",
+      locality: "Downtown",
+      city: "Vancouer",
+      province:"BC",
+      owner: "Rayan",
+      dim: "1350 SqFt",
+      furniture: "Semi-Furnished",
+      dts: " Renting the upstairs portion of the home. At walking distance to the skytrain station"
     },
     {
       id: 5,
       image:
         "https://images.unsplash.com/photo-1622015663319-e97e697503ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1077&q=80",
-      title: "Beach house LakeTown,SK",
-      specs: "3 Beds,1.5 Baths",
-      rent: "$6800 per weeek bitch"
+      title: "Basement",
+      specs: "3 Beds, 1.5 Baths",
+      rent: "6800",
+      locality: "Downtown",
+      city: "Vancouer",
+      province: "BC",
+      owner: "Rayan",
+      dim: "1350 SqFt",
+      furniture: "Semi-Furnished",
+      dts: " Renting the upstairs portion of the home. At walking distance to the skytrain station"
     }
   ];
 
   return (
-    
-      <div className="wrapper-grid">
-        {properties.map((property) => (
-          <PtCard
-            key={property.id}
-            image={property.image}
-            title={property.title}
-            specs={property.specs}
-            rent={property.rent}
-          />
-        ))}
-      </div>
-  
+
+    <div className="wrapper-grid">
+      {properties.map((property) => (
+        <PtCard
+          key={property.id}
+          image={property.image}
+          title={property.title}
+          specs={property.specs}
+          rent={property.rent}
+          locality={property.locality}
+          city={property.city}
+          province={property.province}
+          owner= {property.owner}
+          dim= {property.dim}
+          furniture ={property.furniture}
+          dts= {property.dts}
+
+        />
+      ))}
+    </div>
+
   );
 }
