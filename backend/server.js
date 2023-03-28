f1a019a59d0f463dd2bc561f01d35144dcb385e3
maccimac <macci.web@gmail.com>
2023-03-28 16:18:09 -0700

    Use mongoose for adding and deleting

--- backend/server.js
+++ backend/server.js
@@ -1,87 +1,43 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
 const { MongoClient } = require('mongodb');
 const Property = require('./schemas.js');
+const DB_NAME = "homehunter_db"
+const uri = "mongodb+srv://homehunter:csis@cluster0.hcs7q1d.mongodb.net/" + DB_NAME;
 
+// Parse JSON requests
+app.use(bodyParser.json());
 
-// require('dotenv').config();
+// Connect to MongoDB
+mongoose.connect(uri, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB!');
+});
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-// const uri = process.env.ATLAS_URI;
-const uri = "mongodb+srv://homehunter:csis@cluster0.hcs7q1d.mongodb.net/test";
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true   }
-);
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-
-
-app.get('/properties', (req, res) => {
-    MongoClient.connect(uri, function(err, client) {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Error connecting to database');
-      }
-      const db = client.db('homehunter_db');
-      const properties = db.collection('properties');
-      properties.find().toArray(function(err, results) {
-        if (err) {
-          console.log(err);
-          res.status(500).send('Error retrieving properties from database');
-        }
-        res.send(results);
-        client.close();
-      });
+// :: PROPERTIES ::
+// Get all properties
+app.get('/properties', function(req, res) {
+    Property.find({}, function(err, properties) {
+      if (err) throw err;
+      res.send(properties);
     });
   });
 
-//   app.post('/property', (req, res) => {
-//     MongoClient.connect(uri, function(err, client) {
-//       if (err) {
-//         console.log(err);
-//         res.status(500).send('Error connecting to database');
-//       }
-
-//       const db = client.db('homehunter_db');
-//       const properties = db.collection('properties');
-
-//       properties.insertOne({
-//         address: "123 Main St",
-//         city: "Anytown",
-//         state: "CA",
-//         zip: "12345"
-//       }, function(err, result) {
-//         if (err) {
-//             console.log(err);
-//             res.status(500).send('Error retrieving properties from database');
-//         } else {
-//             res.send(result);
-//         }
-//       });
-//       client.close();
-//     }) // end of mongo client
-    
-//   })
-
-
+// Create a new property
 app.post('/property', function(req, res) {
-const property = new Property({
+  const property = new Property({
     property_id: req.body.property_id,
     category: req.body.category,
     bed: req.body.bed,
     bath: req.body.bath,
     address: {
-    city: req.body.address.city,
-    street: req.body.address.street,
-    zipcode: req.body.address.zipcode,
+      city: req.body.address.city,
+      street: req.body.address.street,
+      zipcode: req.body.address.zipcode,
     },
     furnishing: req.body.furnishing,
     carpet_area_sqm: req.body.carpet_area_sqm,
@@ -92,15 +48,29 @@ const property = new Property({
     imgs: req.body.imgs,
     feature_img: req.body.feature_img,
     rent_price: req.body.rent_price,
-});
+  });
 
-// Save the property to the database
-property.save(function(err) {
+  // Save the property to the database
+  property.save(function(err) {
     if (err) throw err;
     res.send('Property created!');
+  });
 });
+// Delete property
+app.delete('/property/:id', (req, res) => {
+    Property.findOneAndDelete({_id: req.params.id}, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error deleting property from database');
+      }
+      else {
+        res.send(result);
+      }
+    });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+// Start the server
+const port = 5000;
+app.listen(port, function() {
+  console.log(`Server is running on port: ${port}`);
+});
