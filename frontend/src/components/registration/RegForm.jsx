8225bd0d2dd5631c4241c9bf6de04d21288c7350
Aman <amanmishra4191@gmail.com>
2023-04-02 23:39:08 -0700

    added some styling to the registeration page

--- frontend/src/components/registration/RegForm.jsx
+++ frontend/src/components/registration/RegForm.jsx
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const RegistrationForm = () => {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [contactDetails, setContactDetails] = useState("");
+  const [countryCode, setCountryCode] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
   const [photo, setPhoto] = useState("");
   const [errors, setErrors] = useState({});
 
@@ -26,6 +29,8 @@ const RegistrationForm = () => {
     formData.append("firstName", firstName);
     formData.append("lastName", lastName);
     formData.append("contactDetails", contactDetails);
+    formData.append("contactNumber", contactNumber);
+    formData.append("countryCode", countryCode);
     formData.append("photo", photo);
 
     try {
@@ -70,6 +75,9 @@ const RegistrationForm = () => {
     if (!contactDetails) {
       errors.contactDetails = "Contact details are required";
     }
+    if (!contactNumber) {
+      errors.contactNumber = "Contact number is required";
+    }
 
     if (!photo) {
       errors.photo = "Photo is required";
@@ -80,108 +88,133 @@ const RegistrationForm = () => {
     }
 
     return errors;
-    const handleReset = () => {
-      setEmail("");
-      setPassword("");
-      setConfirmPassword("");
-      setFirstName("");
-      setLastName("");
-      setContactDetails("");
-      setPhoto("");
-      setErrors({});
-    };
   };
 
+  const handleReset = () => {
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    setFirstName("");
+    setLastName("");
+    setContactDetails("");
+    setContactNumber("");
+    setPhoto("");
+    setErrors({});
+  };
+
+
   return (
+    <div className="regForm">
+      <form onSubmit={handleSubmit}>
+        <div className="fields">
+          <label htmlFor="email">Email</label>
+          <input className="input"
+            type="email"
+            id="email"
+            value={email}
+            required={true}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          {errors.email && <div className="error">{errors.email}</div>}
+        </div>
+        <div className="row">
+          <div className="fields">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            {errors.password && <div className="error">{errors.password}</div>}
+          </div>
+          <div className="fields">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(event) => setConfirmPassword(event.target.value)}
+            />
+            {errors.confirmPassword && (
+              <div className="error">{errors.confirmPassword}</div>
+            )}
+          </div>
+        </div>
+        <div className="row">
+          <div className="fields">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+            {errors.firstName && <div className="error">{errors.firstName}</div>}
+          </div>
+          <div className="fields">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+            />
+            {errors.lastName && <div className="error">{errors.lastName}</div>}
+          </div>
+        </div>
 
+        <div className="fields">
+          <label htmlFor="contactNumber">Contact Number</label>
+          <div className="contactNo">
+            <select
+              id="countryCode"
+              value={countryCode}
+              onChange={(event) => setCountryCode(event.target.value)}
+            >
+              <option value="+1">+1</option>
+              <option value="+91">+91</option>
+              <option value="+44">+44</option>
+              {/* Add more country codes as needed */}
+            </select>
+            <input
+              type="number"
+              id="contactNumber"
+              value={contactNumber}
+              onChange={(event) => setContactNumber(event.target.value)}
+            />
+          </div>
+          {errors.contactNumber && <div className="error">{errors.contactNumber}</div>}
+        </div>
 
+        <div className="imgBlock">
+          Profile Photo
+          <div className="imgRow">
+            <div className="uploadImg">
+              <label htmlFor="photo">Photo</label>
+              <input
+                type="file"
+                id="photo"
+                onChange={(event) => setPhoto(event.target.files[0])}
+              />
+            </div>
+            <div className="userImg">
+              {photo && (
+                <img
+                  src={URL.createObjectURL(photo)}
+                  alt="Uploaded"
+                  style={{ width: "100%", marginTop: "1rem" }}
+                />
+              )}
+            </div>
+          </div>
+          {errors.photo && <div className="error">{errors.photo}</div>}
+        </div>
+        <button type="submit" >Submit</button>
+      </form>
+    </div>
 
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        {errors.email && <div className="error">{errors.email}</div>}
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        {errors.password && <div className="error">{errors.password}</div>}
-      </div>
-      <div>
-        <label htmlFor="confirmPassword">Confirm Password</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          value={confirmPassword}
-          onChange={(event) => setConfirmPassword(event.target.value)}
-        />
-        {errors.confirmPassword && (
-          <div className="error">{errors.confirmPassword}</div>
-        )}
-      </div>
-      <div>
-    <label htmlFor="firstName">First Name</label>
-    <input
-      type="text"
-      id="firstName"
-      value={firstName}
-      onChange={(event) => setFirstName(event.target.value)}
-    />
-    {errors.firstName && <div className="error">{errors.firstName}</div>}
-  </div>
-  <div>
-    <label htmlFor="lastName">Last Name</label>
-    <input
-      type="text"
-      id="lastName"
-      value={lastName}
-      onChange={(event) => setLastName(event.target.value)}
-    />
-    {errors.lastName && <div className="error">{errors.lastName}</div>}
-  </div>
-  <div>
-    <label htmlFor="contactDetails">Contact Details</label>
-    <textarea
-      id="contactDetails"
-      value={contactDetails}
-      onChange={(event) => setContactDetails(event.target.value)}
-    />
-    {errors.contactDetails && (
-      <div className="error">{errors.contactDetails}</div>
-    )}
-  </div>
-  <div>
-    <label htmlFor="photo">Photo</label>
-    <input
-      type="file"
-      id="photo"
-      onChange={(event) => setPhoto(event.target.files[0])}
-    />
-    {photo && (
-        <img
-          src={URL.createObjectURL(photo)}
-          alt="Uploaded"
-          style={{ width: "100%", marginTop: "1rem" }}
-        />
-      )}
-    {errors.photo && <div className="error">{errors.photo}</div>}
-  </div>
-  <button type="submit" disabled={!canSubmit}>Submit</button>
-</form>
-
-
-       
-);
+  );
 };
 
 export default RegistrationForm;
