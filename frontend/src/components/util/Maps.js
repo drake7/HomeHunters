50b267e9dcba684529808b3851a00be42d5d9224
maccimac <macci.web@gmail.com>
2023-04-11 16:21:30 -0700

    Changes for the maps and property pages

---
+++ frontend/src/components/util/Maps.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import {Client} from "@googlemaps/google-maps-services-js";
+
+import { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+
+class Maps extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '',
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    };
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+
+  render() {
+    const { address } = this.props;
+    console.log("hi",  this.props.address);
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        className="maps"
+        initialCenter={{ lat: this.props.lat, lng: this.props.lang }}
+        onClick={this.onMapClicked}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Current Location'}
+          position={{ lat: this.props.lat, lng: this.props.lang }}
+        />
+      </Map>
+        
+    );
+    
+  }
+}
+
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCeRHKIdbLhizyMV0D6Gr0rHEPXc427jtE'
+})(Maps);
